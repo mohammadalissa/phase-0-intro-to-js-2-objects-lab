5b193e590731289e88da7026916134b2dc0a3ee9
Mohammad Al Issa <mohammed.alissa627@gmail.com>
2023-09-26 21:12:26 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+let employee = {name:"wasfi",streetAddress:'sjmamma'}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newemployee = { ...employee };
+    newemployee[key] = value;
+    return newemployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee
+
+}
+function deleteFromEmployeeByKey(employee, key){
+    const newemployee = { ...employee };
+    delete newemployee[key]
+    return newemployee
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+}
